7f0b87b6469cf44a4d01d1a02af75e14f4cd694f
David Kolia <Dkolia@icloud.com>
2018-08-27 13:21:35 -0500

    Added Errors and register form functionality

--- routes/users.js
+++ routes/users.js
@@ -13,5 +13,32 @@ router.get('/login', function(req, res){
 });
 
 
+// Register Users
+router.post('/register', function(req, res){
+   const name = req.body.name;
+   const email = req.body.email;
+   const username = req.body.username;
+   const password = req.body.password;
+   const password2 = req.body.password2;
+
+   // User Validation
+   req.checkBody('name', 'Name is required').notEmpty();
+   req.checkBody('email', 'Email is required').notEmpty();
+   req.checkBody('email', 'Email is invalid').isEmail();
+   req.checkBody('username', 'Username is required').notEmpty();
+   req.checkBody('password', 'A Password is required').notEmpty();
+   req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+   var errors = req.validationErrors();
+
+   if(errors){
+     res.render('register',{
+       errors: errors
+     });
+   } else {
+     console.log("no errors");
+   }
+});
+
 
 module.exports = router;
